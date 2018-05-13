495ad2abfce2fa310c4c87f4489f96d562c2a11f
monycky <monyckyfrazao@gmail.com>
2018-05-13 18:25:21 -0300

    add: basic components screen

--- dragGameLevel.lua
+++ dragGameLevel.lua
@@ -16,21 +16,20 @@ local dragGameLevelFourButton
 
 local backToActivities
 
-
 local function goTodragGameLevelOne()
-	composer.gotoScene("dragGameLevel", {effect = "slideLeft", time = 500})
+	composer.gotoScene("dragGameLevelOne", {effect = "slideLeft", time = 500})
 end
 
 local function goTodragGameLevelTwo()
-    composer.gotoScene("dragGameLevel", {effect = "slideLeft", time = 500})
+    composer.gotoScene("dragGameLeveTwo", {effect = "slideLeft", time = 500})
 end
 
 local function goTodragGameLevelThree()
-    composer.gotoScene("dragGameLevel", {effect = "slideLeft", time = 500})
+    composer.gotoScene("dragGameLevelThree", {effect = "slideLeft", time = 500})
 end
 
 local function goTodragGameLevelFour()
-    composer.gotoScene("dragGameLevel", {effect = "slideLeft", time = 500})
+    composer.gotoScene("dragGameLevelFour", {effect = "slideLeft", time = 500})
 end
 
 local function goToActivities()
@@ -45,34 +44,27 @@ function scene:create( event )
     dragGameLevelBg = display.newImageRect("background.png", 1050, 700)
     sceneGroup:insert(dragGameLevelBg)
 
+
+    dragGameLevelOneButton = display.newImage('level1.png', 100, 170) -- OK
+    sceneGroup:insert(dragGameLevelOneButton)
+    dragGameLevelOneButton: addEventListener("tap", goTodragGameLevelFour)
+
+    dragGameLevelTwoButton = display.newImage('level2.png', 190, 170) -- OK
+    sceneGroup:insert(dragGameLevelOneButton)
+    dragGameLevelOneButton: addEventListener("tap", goTodragGameLevelTwo)
+
+    dragGameLevelThreeButton = display.newImage('level3.png', 290, 170) -- OK
+    sceneGroup:insert(dragGameLevelOneButton)
+    dragGameLevelOneButton: addEventListener("tap", goTodragGameLevelThree)
+
+    dragGameLevelFourButton = display.newImage('level4.png', 390, 170) -- OK
+    sceneGroup:insert(dragGameLevelOneButton)
+    dragGameLevelOneButton: addEventListener("tap", goTodragGameLevelFour)
+
+
     dragGameLevelTitle = display.newText("Drag Game Levels", 235, 90, "", 30)
     dragGameLevelTitle:setFillColor(255, 255, 255)
     sceneGroup:insert(dragGameLevelTitle)
-
-
-    dragGameLevelOneButton = display.newRect(display.contentCenterX, display.contentHeight*.7, display.contentWidth*.2, display.contentHeight*.15)
-    dragGameLevelOneButton:setFillColor(255/225, 100/255, 120/255)
-    sceneGroup:insert(dragGameLevelOneButton)
-    dragGameLevelOneButton: addEventListener("tap", goTodragGameLevelOne)
-
-
-    dragGameLevelTwoButton = display.newRect(display.contentCenterX, display.contentHeight*.5, display.contentWidth*.2, display.contentHeight*.15)
-    dragGameLevelTwoButton:setFillColor(255/225, 100/255, 120/255)
-    sceneGroup:insert(dragGameLevelTwoButton)
-    dragGameLevelTwoButton: addEventListener("tap", goTodragGameLevelTwo)
-
-
-    dragGameLevelThreeButton = display.newRect(display.contentCenterX, display.contentHeight*.3, display.contentWidth*.2, display.contentHeight*.15)
-    dragGameLevelThreeButton:setFillColor(255/225, 100/255, 120/255)
-    sceneGroup:insert(dragGameLevelThreeButton)
-    dragGameLevelThreeButton: addEventListener("tap", goTodragGameLevelThree)
-
-
-    dragGameLevelFourButton = display.newRect(display.contentCenterX, display.contentHeight*.9, display.contentWidth*.2, display.contentHeight*.15)
-    dragGameLevelFourButton:setFillColor(255/225, 100/255, 120/255)
-    sceneGroup:insert(dragGameLevelFourButton)
-    dragGameLevelFourButton: addEventListener("tap", goTodragGameLevelFour)
-   
 
     backToHomeButton = display.newRect(display.contentCenterX, display.contentHeight*.11, display.contentWidth*.2, display.contentHeight*.10)
     backToHomeButton:setFillColor(255/255, 255/255, 0/255)
@@ -117,8 +109,13 @@ end
 function scene:destroy( event )
  
     local sceneGroup = self.view
-    sceneGroup:remove(playButton)
-    sceneGroup:remove(creditButton)
+    sceneGroup:remove(dragGameLevelOneButton)
+    sceneGroup:remove(goTodragGameLevelTwo)
+    sceneGroup:remove(dragGameLevelThreeButton)
+    sceneGroup:remove(dragGameLevelFourButton)
+
+    sceneGroup:remove(backToActivities)
+
  
 end
  
