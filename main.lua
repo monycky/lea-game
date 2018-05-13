1588a86b9925a0720a75b0f8114189d8dc945206
monycky <monyckyfrazao@gmail.com>
2018-05-13 15:21:51 -0300

    change: componser call

--- main.lua
+++ main.lua
@@ -2,5 +2,5 @@ local composer = require("composer")
 
 display.setStatusBar(display.HiddenStatusBar)
 
-composer.gotoScene("activities", {effect = "fade", time = 500})
+composer.gotoScene("home", {effect = "fade", time = 500})
 
